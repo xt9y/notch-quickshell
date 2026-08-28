c7c63807d35745a1ca0139a6548788c34f5c3b7f
xt9y <felix@xt9y.de>
2026-08-28 02:42:11 +0200

    Update config-update helper for Plasma

--- shell/notch-aliases.sh
+++ shell/notch-aliases.sh
@@ -1,4 +1,4 @@
 # Shell helpers managed by xt9y/notch-quickshell.
-# This file is sourced from ~/.bashrc by scripts/apply-hypr.sh.
+# This file is sourced from ~/.bashrc or ~/.zshrc by scripts/setup-plasma.sh.
 
 alias config-update='setsid -f bash "$HOME/.config/quickshell/notch/scripts/config-update.sh" "$HOME/.config/quickshell/notch" >/dev/null 2>&1'
