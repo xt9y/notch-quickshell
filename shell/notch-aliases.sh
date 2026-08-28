4af0678604aa1ec2b18d4d875e5194fb4d583d83
xt9y <felix@xt9y.de>
2026-08-28 02:48:58 +0200

    Remove config-update alias

--- shell/notch-aliases.sh
+++
@@ -1,4 +0,0 @@
-# Shell helpers managed by xt9y/notch-quickshell.
-# This file is sourced from ~/.bashrc or ~/.zshrc by scripts/setup-plasma.sh.
-
-alias config-update='setsid -f bash "$HOME/.config/quickshell/notch/scripts/config-update.sh" "$HOME/.config/quickshell/notch" >/dev/null 2>&1'
