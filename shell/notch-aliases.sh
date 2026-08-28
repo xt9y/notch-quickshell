8f3d23c3ca0b5a3d899347e82b58b829804b5c0d
xt9y <felix@xt9y.de>
2026-08-28 02:14:09 +0200

    Route config-update alias through update script

--- shell/notch-aliases.sh
+++ shell/notch-aliases.sh
@@ -1,4 +1,4 @@
 # Shell helpers managed by xt9y/notch-quickshell.
 # This file is sourced from ~/.bashrc by scripts/apply-hypr.sh.
 
-alias config-update='cd "$HOME/.config/quickshell/notch" && git pull && setsid -f qs -c notch >/dev/null 2>&1'
+alias config-update='setsid -f bash "$HOME/.config/quickshell/notch/scripts/config-update.sh" "$HOME/.config/quickshell/notch" >/dev/null 2>&1'
